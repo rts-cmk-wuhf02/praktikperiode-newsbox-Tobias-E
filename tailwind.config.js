75d445c5d7be46b901f41b69140f9acc3e182202
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-03-27 11:22:39 +0100

    UI added

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+	theme: {
+		extend: {
+			borderRadius: {
+				'12px': '0.75rem'
+			},
+			fontSize: {
+				'30px': '1.875rem',
+				'20px': '1.25rem',
+				'18px': '1.125rem',
+				'15px': '0.9375rem',
+				'14px': '0.875rem',
+				'12px': '0.75rem'
+			},
+			colors: {
+				primary: {
+					sage: '#87BCBF',
+					rust: '#D97D54',
+					drab: '#324755',
+					danger: '#D95454'
+				},
+				secondary: {
+					ice: '#F0F3F4',
+					fossil: '#C8D1D3',
+					sand: '#B9B0A2'
+				},
+				typography: {
+					onyx: '#1B1C20',
+					slate: '#6E8CA0',
+					snow: '#FFFFFF',
+					pblue: '#6E8CA0',
+					grey: '#707070'
+				},
+				utility: {
+					bordergrey: '#E0E1E2',
+					inputgrey: '#F0F3F4',
+					settingsbg: '#ECEFF0'
+				}
+			}
+		}
+	},
+	variants: {},
+	plugins: []
+};
