6bc08ecb7cafd92c91ce75ba79b4d0939302214c
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-04-04 11:15:24 +0200

    dark mode, few changes in settings

--- src/javascript/getTheme.js
+++ src/javascript/getTheme.js
@@ -1,5 +1,3 @@
-// Queries
-
 function detectColorTheme() {
 	const fetchTheme = window.localStorage.getItem('theme');
 
@@ -12,60 +10,3 @@ function detectColorTheme() {
 	}
 }
 detectColorTheme();
-/* 	if (fetchTheme) {
-		if (fetchTheme == 'dark') {
-			theme = 'dark';
-		}
-	} else if (!window.matchMedia) {
-		// match media not supported
-	} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		theme = 'dark';
-	} */
-
-//let preferablyTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-/* let theme = window.localStorage.getItem('theme');
-
-console.log(`localstorage: ${window.localStorage.getItem('theme')}`);
-console.log(`dataset-theme: ${(document.documentElement.dataset.theme = theme)}`);
-if (theme) {
-	document.documentElement.dataset.theme = theme;
-} */
-
-/* btn.addEventListener('click', () => {
-	if (document.documentElement.dataset.theme == 'light') {
-		document.documentElement.dataset.theme = 'dark';
-		window.localStorage.setItem('theme', 'dark');
-		btn.innerHTML = 'Toggle Light Mode';
-		console.log('Theme : Dark');
-	} else {
-		document.documentElement.dataset.theme = 'light';
-		window.localStorage.setItem('theme', 'light');
-		btn.innerHTML = 'Toggle Dark Mode';
-		console.log('Theme : Light');
-	}
-}); */
-
-/* if (theme == null) {
-	if (preferablyTheme) {
-		document.documentElement.dataset.theme = 'dark';
-		window.localStorage.setItem('theme', 'dark');
-	} else {
-		document.documentElement.dataset.theme = 'light';
-		window.localStorage.setItem('theme', 'light');
-	}
-} else if (preferablyTheme && theme == 'light') {
-	document.documentElement.dataset.theme = 'light';
-	// window.localStorage.setItem('theme', 'light');
-} else {
-	document.documentElement.dataset.theme = 'light';
-	window.localStorage.setItem('theme', 'light');
-} */
-
-/*
-    <script>
-        let themes = window.localStorage.getItem('theme');
-        if (themes) {
-            document.documentElement.dataset.theme = themes;
-        }
-        console.log(`localstorage: ${window.localStorage.getItem('theme')}`);
-    </script> */
